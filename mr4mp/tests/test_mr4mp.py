fc45788497c1ace0b9c72e043c0032b0b0181512
Andrei Lapets <a@lapets.io>
2018-01-01 22:29:25 -0500

    Combine two test scripts.

--- mr4mp/tests/test_mr4mp.py
+++ mr4mp/tests/test_mr4mp.py
@@ -1,5 +1,6 @@
 from random import choice
 from string import ascii_lowercase
+from timeit import default_timer
 from unittest import TestCase
 import mr4mp
 
@@ -13,8 +14,13 @@ def merge(i, j):
     return {k:(i.get(k,set()) | j.get(k,set())) for k in i.keys() | j.keys()}
 
 class TestPool(TestCase):
-    def test_all(self):
-        result = mr4mp.pool().mapreduce(index, merge, range(100))
+    def test_mapreduce(self):
+        pool = mr4mp.pool()
+        print("Starting.")
+        start = default_timer()
+        result = pool.mapreduce(index, merge, range(100))
+        print("Finished in " + str(default_timer()-start) +
+              "s using " + str(len(pool)) + " processes.")
         self.assertEqual(type(result), dict)
 
 ## eof

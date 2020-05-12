9ee33f0d20bec16bc39115c8ab3745fa5b43f2b5
Andrei Lapets <a@lapets.io>
2020-05-11 23:59:17 -0400

    Update package file and .gitignore.

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name             = "mr4mp",
-    version          = "0.0.4.2",
-    packages         = ["mr4mp",],
-    install_requires = ["parts",],
-    license          = "MIT",
-    url              = "https://github.com/lapets/mr4mp",
-    author           = "Andrei Lapets",
-    author_email     = "a@lapets.io",
-    description      = "Thin MapReduce-like layer on top of the Python multiprocessing library.",
-    long_description = long_description,
-    long_description_content_type = "text/x-rst",
-    test_suite       = "nose.collector",
-    tests_require    = ["nose"],
+    name="mr4mp",
+    version="0.0.5.0",
+    packages=["mr4mp",],
+    install_requires=["parts",],
+    license="MIT",
+    url="https://github.com/lapets/mr4mp",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Thin MapReduce-like layer on top of the "+\
+                "Python multiprocessing library.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
 )
